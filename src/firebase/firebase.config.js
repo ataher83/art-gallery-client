1bed7666538398d3535cbeea3daab46d1e6db455
ataher83 <taher83@gmail.com>
2024-04-26 14:31:47 +0600

    Environment variable to hide the Firebase config keys applied

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,19 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAIJvcJkR5VUGTrXQJqPv0ozF01FmnEoUI",
-  authDomain: "the-art-gallery-74571.firebaseapp.com",
-  projectId: "the-art-gallery-74571",
-  storageBucket: "the-art-gallery-74571.appspot.com",
-  messagingSenderId: "257907237444",
-  appId: "1:257907237444:web:0138f1d153a2b6bd195211"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
+
+//   apiKey: "AIzaSyAIJvcJkR5VUGTrXQJqPv0ozF01FmnEoUI",
+//   authDomain: "the-art-gallery-74571.firebaseapp.com",
+//   projectId: "the-art-gallery-74571",
+//   storageBucket: "the-art-gallery-74571.appspot.com",
+//   messagingSenderId: "257907237444",
+//   appId: "1:257907237444:web:0138f1d153a2b6bd195211"
 };
 
 // Initialize Firebase
