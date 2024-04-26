64d155684c47b159382eb5b7dc003d987f2e67c5
ataher83 <taher83@gmail.com>
2024-04-26 17:06:43 +0600

    navbar updated

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -65,7 +65,7 @@ const Navbar = () => {
                                 
                                 <div>
                                     <button onClick={handleSignOut}
-                                    className="btn">Sign Out</button>
+                                    className="btn">Log out</button>
                                 </div>
                             </div>
                         </div>
